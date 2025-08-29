ba0a82f486010c0a2b157aa432c84fc3bd285b22
juliapinat <julia.pinat@escola.pr.gov.br>
2025-08-29 12:14:26 -0300

    Add files via upload

---
+++ Julia2tec/receita.js
@@ -0,0 +1,15 @@
+//lista de ingredientes// 
+const ingredientes = [
+    "3 ovos",
+    "2 xicaras de farinha de trigo",
+    "1/2 xicara de oleo",
+    "1 xicara de leite",
+    "1 colher de sopa de fermento em pó"
+];
+
+//titulo
+console.log("📌 Receita de Bolo Simples");
+console.log("Ingredientes:");
+
+//exibindo cada ingrediente da lista
+ingredientes.forEach((item,index))
